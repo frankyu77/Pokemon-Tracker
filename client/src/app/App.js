e8090b9b86f8a65e322d029a36d477f65265fc1f
Ekenny02 <ekenny456@gmail.com>
2024-03-21 13:53:45 -0700

    Completed Data Display Structure

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,12 +3,58 @@ import EntityDisplay from './components/EntityDisplay';
 
 function App() {
 
-  const pokemonData = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
+  const gameAttributes = ['Game ID', 'Difficulty', 'Generation']
+  const gameData = [['a', 'b', 'c'], ['d', 'e', 'f']]
+
+  const pokemonAttributes = ['Name', 'Type', 'Weakness', 'Special Attack']
+  const pokemonData = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']]
+
+  const itemsAttributes = ['Item #', 'Rarity']
+  const itemsData = [['a', 'b'], ['c', 'd']]
+
+  const peopleAttributes = ['Pid', 'Name']
+  const peopleData = [['a', 'b'], ['c', 'd']]
+
+  const gymsAttributes = ['Gym #', 'Type', 'Difficulty']
+  const gymsData = [['a', 'b', 'c'], ['d', 'e', 'f']]
+
+  const gymMastersAttributes = ['Pid', 'Name', 'Badge']
+  const gymMastersData = [['a', 'b', 'c'], ['d', 'e', 'f']]
+
+  const npcsAttributes = ['Pid', 'Name', 'Role', 'Catch Phrase']
+  const npcsData = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']]
+
+  const trainersAttributes = ['Pid', 'Name', 'Favourite Pokemon']
+  const trainersData = [['a', 'b', 'c'], ['d', 'e', 'f']]
+
+  const regionsAttributes = ['Name', 'Type']
+  const regionsData = [['a', 'b'], ['c', 'd']]
+
+  const questsAttributes = ['Quest Id', 'Difficulty', 'Reward']
+  const questsData = [['a', 'b', 'c'], ['d', 'e', 'f']]
+
+  const enterableAreasAttributes = ['Area #', 'Type']
+  const enterableAreasData = [['a', 'b'], ['c', 'd']]
 
   return (
-    <div class='app'>
-      <h1 class='title'>Pokemon Game</h1>
-      <EntityDisplay class='pokemon-entity' name='Pokemon' data={pokemonData}/>
+    <div id='app'>
+      <h1 id='title'>Pokemon Game</h1>
+      <div id="data-display">
+        <EntityDisplay name='game' attributes={gameAttributes} data={gameData}/>
+        <EntityDisplay name='pokemon' attributes={pokemonAttributes} data={pokemonData}/>
+        <EntityDisplay name='items' attributes={itemsAttributes} data={itemsData}/>
+
+        <EntityDisplay name='people' attributes={peopleAttributes} data={peopleData}/>
+        <EntityDisplay name='gyms' attributes={gymsAttributes} data={gymsData}/>
+        <EntityDisplay name='gym masters' attributes={gymMastersAttributes} data={gymMastersData}/>
+
+        <EntityDisplay name='npcs' attributes={npcsAttributes} data={npcsData}/>
+        <EntityDisplay name='trainers' attributes={trainersAttributes} data={trainersData}/>
+        <EntityDisplay name='regions' attributes={regionsAttributes} data={regionsData}/>
+
+        <EntityDisplay name='quests' attributes={questsAttributes} data={questsData}/>
+        <EntityDisplay name='enterable areas' attributes={enterableAreasAttributes} data={enterableAreasData}/>
+      </div>
     </div>
   );
 }
--- client/src/app/components/EntityDisplay.js
+++ client/src/app/components/EntityDisplay.js
@@ -1,10 +1,27 @@
 import './EntityDisplay.css'
 
 function EntityDisplay(props) {
+    return (
+        <div id={props.name} class="entity">
+            <h2 id="name">{props.name}</h2>
+            <table id="data"> {populateTable(props.attributes, props.data)} </table>
+            <div id="buttons">
+                <button id="add-button">Add</button>
+                <button id="remove-button">Remove</button>
+            </div>
+        </div>
+    );
+}
 
-    const rawData = props.data
+const populateTable = (attributes, rawData) => {
+    const table = [];
+    const tableHeaders = [];
 
-    const table = []
+    for (const attribute of attributes) {
+        tableHeaders.push(<th>{attribute}</th>)
+    }
+
+    table.push(<tr> {tableHeaders} </tr>)
 
     for (const dataRow of rawData) {
 
@@ -17,12 +34,7 @@ function EntityDisplay(props) {
         table.push(<tr> {tableRow} </tr>)
     }
 
-    return (
-        <div id="entity">
-            <h2 id="name">{props.name}</h2>
-            <table id="data"> {table} </table>
-        </div>
-    );
-}
+    return table;
+};
 
 export default EntityDisplay
