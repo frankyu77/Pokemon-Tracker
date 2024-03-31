3fc6a04cf29961fbdf44cdf68d0cb27c01c39689
pmacau <pmacau@students.cs.ubc.ca>
2024-03-30 21:55:18 -0700

    added all tests now

--- server/tables/pokemonService.js
+++ server/tables/pokemonService.js
@@ -29,6 +29,20 @@ class pokemonService {
             console.error('Error removing game:', err);
         }
     }
+    async countPokemon(){
+        const sql = 'SELECT COUNT * FROM POKEMON_CAUGHT'
+
+        try {
+            const result = await this.db.executeQuery(sql, bindings); 
+            console.log(`Pokemon count worked`);
+            return result;
+        } catch (err) {
+            console.error('Error inserting pokemon:', err);
+            return false;
+        }
+
+
+    }
 }
 
 module.exports = pokemonService;
